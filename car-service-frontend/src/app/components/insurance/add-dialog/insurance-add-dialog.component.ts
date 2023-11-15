b804c397017fba7943b0fc265b79e0bb5ee7e2da
Mateusz <mtpala@interia.pl>
2023-11-15 09:21:11 +0100

    fix zakładki dodawania ubezpieczeń

--- car-service-frontend/src/app/components/insurance/add-dialog/insurance-add-dialog.component.ts
+++ car-service-frontend/src/app/components/insurance/add-dialog/insurance-add-dialog.component.ts
@@ -1,25 +1,49 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
 import {FormGroup} from '@angular/forms';
 import {InsuranceAddDialogFormService} from './insurance-add-dialog-form.service';
 import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
 import {DictionaryService, DictionaryType} from '../../../services/dictionary.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'insurance-add-dialog',
   templateUrl: './insurance-add-dialog.component.html',
   styleUrls: ['./insurance-add-dialog.component.scss']
 })
-export class InsuranceAddDialogComponent {
+export class InsuranceAddDialogComponent implements OnInit, OnDestroy {
   form: FormGroup;
   companies = this.getDictionary(DictionaryType.INSURANCE_COMPANIES);
   acProtectionTypes = this.getDictionary(DictionaryType.AC_PROTECTION_TYPES);
   loanPartsAmounts = this.getDictionary(DictionaryType.LOAN_PARTS_AMOUNT);
+  acSubscription: Subscription = new Subscription();
 
   constructor(private dialogRef: MatDialogRef<any>,
               private formService: InsuranceAddDialogFormService,
               private dictionaryService: DictionaryService,
               @Inject(MAT_DIALOG_DATA) private vehicleId: string) {
     this.form = this.formService.getFormGroup();
+  }
+
+  ngOnInit() {
+    this.startAcSubscription();
+  }
+
+  ngOnDestroy() {
+    this.acSubscription.unsubscribe();
+  }
+
+  startAcSubscription() {
+   this.acSubscription = this.formService.getAcControl(this.form).valueChanges.subscribe({
+     next: (value) => {
+       const acProtectionTypesControl = this.formService.getAcProtectionTypesControl(this.form);
+      if (value) {
+        acProtectionTypesControl.enable();
+      } else {
+        acProtectionTypesControl.setValue(null);
+        acProtectionTypesControl.disable();
+      }
+     }
+   });
   }
 
   cancel() {
