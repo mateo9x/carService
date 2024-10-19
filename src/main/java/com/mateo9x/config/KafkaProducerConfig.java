fb5197591bfe773450828853fbf3ca428d5abb0d
mateo9x <mtpala@interia.pl>
2024-10-19 18:41:38 +0200

    Send mail if kafka profile enabled

--- src/main/java/com/mateo9x/config/KafkaProducerConfig.java
+++ src/main/java/com/mateo9x/config/KafkaProducerConfig.java
@@ -6,6 +6,7 @@ import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.DefaultKafkaProducerFactory;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.core.ProducerFactory;
@@ -15,6 +16,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @Configuration
+@Profile("kafka")
 @RequiredArgsConstructor
 public class KafkaProducerConfig {
 
--- src/main/java/com/mateo9x/config/KafkaTopicConfig.java
+++ src/main/java/com/mateo9x/config/KafkaTopicConfig.java
@@ -4,11 +4,13 @@ import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.admin.NewTopic;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.kafka.core.KafkaAdmin;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Profile("kafka")
 @Configuration
 public class KafkaTopicConfig {
 
